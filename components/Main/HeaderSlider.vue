51e65e88588f196016c18a5a6a4396dee7881d19
erfan moghadasi <erfan.moqadasii@gmail.com>
2023-03-15 22:39:30 +0330

    fix: main page responsive

--- components/Main/HeaderSlider.vue
+++ components/Main/HeaderSlider.vue
@@ -1,5 +1,5 @@
 <template>
-  <div dir="ltr" class="relative w-full h-[690px] m-xl:h-[550px]">
+  <div dir="ltr" class="relative w-full h-[690px] m-xl:h-[550px] pt-16 2xl:pt-24">
     <ClientOnly>
       <swiper
         :modules="[Autoplay, Navigation]"
@@ -18,26 +18,26 @@
           <div>
             <nuxt-img
               :src="slide.img"
-              class="m-xl:scale-75"
+              class="m-xl:scale-[.70]"
               height="450"
               width="370"
             />
           </div>
           <div
-            class="h-full max-w-[650px] m-xl:max-w-[550px] items-end text-right flex flex-col justify-center gap-8"
+            class="h-full max-w-[650px] m-xl:max-w-[550px] items-end text-right flex flex-col justify-center gap-8 m-xl:gap-6 m-xl:pr-5"
           >
-            <h3 class="font-black text-5xl m-xl:text-4xl leading-[60px]">
+            <h3 class="font-black text-5xl m-xl:text-3xl leading-[60px]">
               <p class="mb-4">پلتفــــرم</p>
               {{ slide.title }}
               <span class="text-[#72FFD5]"> محصولات دیجیتال</span>
             </h3>
-            <p class="font-light text-xl m-xl:text-lg text-gray-300">
+            <p class="font-light text-xl m-xl:text-base text-gray-300">
               {{ slide.description }}
             </p>
             <button
-              class="c-btn-white flex items-center justify-center gap-2 bg-white w-44 h-14 text-primary-orginal text-xl font-bold"
+              class="c-btn-white m-xl:h-12 flex items-center justify-center gap-2 bg-white w-44 m-xl:w-36 h-14 text-primary-orginal text-xl m-xl:text-base font-bold"
             >
-            <NuxtIcon name="main/arrow-left-header" filled class="text-2xl" />
+            <NuxtIcon name="main/arrow-left-header" filled class="text-2xl m-xl:text-lg" />
               <span>{{ slide.buttonTitle }}</span>
             </button>
           </div>
@@ -46,12 +46,12 @@
       <NuxtIcon
         name="swiper/arrow-header-left"
         filled
-        class="next-el-new text-[40px] absolute top-[50%] right-5 cursor-pointer z-10"
+        class="next-el-new text-[40px] absolute top-[50%] right-5 cursor-pointer z-10 m-xl:scale-75"
       />
       <NuxtIcon
         name="swiper/arrow-header-right"
         filled
-        class="prev-el-new text-[40px] absolute top-[50%] left-5 cursor-pointer z-10"
+        class="prev-el-new text-[40px] absolute top-[50%] left-5 cursor-pointer z-10 m-xl:scale-75"
       />
     </ClientOnly>
   </div>
--- components/Main/SpecialBox.vue
+++ components/Main/SpecialBox.vue
@@ -1,43 +1,45 @@
 <template>
   <div class="max-w-[1558px] m-xl:mx-20  mx-auto my-28  flex items-center gap-6">
-    <div class="w-full h-[300px] rounded-[10px] bg-[#F5F5F5] flex justify-between">
-      <div class="flex flex-col justify-center gap-8 px-14">
+    <div class="w-full h-[300px] m-xl:h-[240px] rounded-[10px] bg-[#F5F5F5] flex justify-between">
+      <div class="flex flex-col justify-center gap-8 m-xl:gap-3 px-14">
         <h4 class="font-black text-4xl xl:text-2xl">
-          فروش ویژه کنسول گیمینگ و محصولات اپل
+          فروش ویژه <span class="text-[#1146C8]">کنسول گیمینگ</span> و محصولات <span class="text-[#EC641E]">اپل</span>
         </h4>
         <p class="text-xl  xl:text-base text-[#747474]">
           لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم
         </p>
         <button
-          class="flex items-center justify-center gap-2 rounded-[40px] w-32 h-12 font-bold text-xl text-white bg-gradient-to-tr from-[#76D2FF] to-[#0181C4]"
+          class="flex items-center justify-center gap-2 rounded-[40px] w-32 m-xl:w-24 h-12 m-xl:h-10 m-xl:text-sm font-bold text-xl text-white bg-gradient-to-tr from-[#76D2FF] to-[#0181C4]"
         >
           <span>مشاهده</span>
           <NuxtIcon name="main/black-arrow-left" />
         </button>
       </div>
       <nuxt-img
-        src="/images/box-category/iphone.png"
+        src="/images/main/iphone.png"
         height="full"
         width="full"
         class="mx-8 my-5"
       />
     </div>
     <div
-      class="w-full h-[300px] rounded-[10px] left-box flex items-center justify-between overflow-hidden"
+      class="w-full h-[300px] m-xl:h-[240px] rounded-[10px] left-box flex items-center justify-between overflow-hidden"
     >
-      <div class="flex flex-col justify-center gap-8 px-14">
-        <h4 class="font-black text-4xl xl:text-2xl">هندزفری بیسیم شـــــــارک</h4>
+      <div class="flex flex-col justify-center gap-8  m-xl:gap-3 px-14">
+        <h4 class="font-black text-4xl m-xl:text-2xl ">هندزفری بیسیم <span class="m-xl:text-3xl text-[#0181C4]">شـــــــارک</span></h4>
         <p class="text-xl xl:text-base text-[#1E1E1E]">
           لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ
         </p>
         <button
-          class="flex items-center justify-center gap-2 rounded-[40px] w-32 h-12 font-bold text-xl text-white bg-gradient-to-tr from-[#76D2FF] to-[#0181C4]"
+          class="flex items-center justify-center gap-2 rounded-[40px] w-32 m-xl:w-24 h-12 m-xl:h-10 m-xl:text-sm font-bold text-xl text-white bg-gradient-to-tr from-[#76D2FF] to-[#0181C4]"
         >
           <span>مشاهده</span>
           <NuxtIcon name="main/black-arrow-left" />
         </button>
       </div>
-      <nuxt-img src="/images/main/headphone.png" height="full" />
+      <div class="h-full aspect-square">
+        <nuxt-img src="/images/main/headphone.png" fit="fill"  class="" />
+      </div>
     </div>
   </div>
 </template>
